03aafc05c0a0ae3a90b7314256f48449e0de03f8
AryaEm <ary4maulanaa@gmail.com>
2024-08-21 13:16:23 +0700

    add controller and routes

---
+++ src/routes/route.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+const praaktikumController = require(`../controllers/praktikumController`)
+
+app.get("/profil/:name/:age/:kelas", praaktikumController.profil)
+app.get("/identitas", praaktikumController.identitas)
+
+module.exports = app
